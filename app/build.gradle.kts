a484495f0e6e5d5ebba11a9cc329214ac0244434
Akash Yadav <itsaky01@gmail.com>
2023-03-20 23:14:21 +0530

    build(app): migrate to kotlin script

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.app)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.farmbuddy"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.farmbuddy"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName =  "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.google.material)
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
